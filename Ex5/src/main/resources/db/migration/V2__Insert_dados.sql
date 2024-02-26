9f8b8fd14d069e10b973644240ed6fa4653e4e35
Fabio F Silva <fabiofsilva92@gmail.com>
2024-02-25 23:37:39 -0300

    ajuste no script flyway

--- Ex5/src/main/resources/db/migration/V2__Insert_dados.sql
+++ Ex5/src/main/resources/db/migration/V2__Insert_dados.sql
@@ -1,4 +1,4 @@
 INSERT INTO veiculos (veiculo, marca, ano, descricao, vendido, created, updated)
 VALUES
-('Modelo X', 'Marca Y', 2020, 'Descrição do veículo Modelo X', FALSE, NOW(), NOW()),
-('Modelo Z', 'Marca W', 2019, 'Descrição do veículo Modelo Z', TRUE, NOW(), NOW());
+('Civic', 'Honda', 2020, 'bem bonito', FALSE, NOW(), NOW()),
+('Camry', 'Toyota', 2019, 'bonito pra caramba', TRUE, NOW(), NOW());
